a6bc6d712a0f5e1931ac1e6664f32842c2d0d9f7
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-01-20 23:56:23 +0600

    learned asset and changing app icon

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    backgroundColor: Colors.blueGrey,
-    appBar: AppBar(
-      title: Text('I Am Rich'),
-      backgroundColor: Colors.blueGrey[900],
-    ),
-    body: const Center(
-      child: Image(
-        image: NetworkImage(
-            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-      ),
-    ),
-  )));
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: const Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: const Center(
+            child: Image(
+              image: AssetImage("images/diamond.png"),
+            ),
+          ))));
 }
+
+//Image(
+//image: AssetImage(images / screenshot.png),
+// image: NetworkImage(
+//     'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
